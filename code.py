4cca5dbff2d9c7c165c46c57e334588b8f63d365
Kazi <adnana.kazi05@gmail.com>
2025-09-17 22:03:06 -0400

    Added gold as a Df

--- code.py
+++ code.py
@@ -1,17 +1,27 @@
 import yfinance as yf
 import pandas as pd
-
-#This commmand allows me to view more rows of the data
-pd.set_option('display.max_rows', None)
+import matplotlib.pyplot as plt
 
 # This will download the S&P 500 index data
 # The index data is represented by the ticker symbol "^GSPC"
 # A ticker is a unique identifier for a particular stock or index on the stock market
 sp500 = yf.Ticker("^GSPC")
+GOLD = yf.Ticker("GLD")
 
 # Next well find the history of the data from the S&P 500 index
-sp500 = sp500.history(period="max") 
+sp500 = sp500.history(period="max")
+GOLD = GOLD.history(period="max") 
 
 df = pd.DataFrame(sp500)
 print(df)
+
+df2 = pd.DataFrame(GOLD)
+print(df2)
+
+sp500.plot.line(y="Close", use_index=True)
+plt.show()
+
+GOLD.plot.line(y="Close", use_index=True)
+plt.show()
+
 
